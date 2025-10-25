00b383ddf4d1fccdc6ffcf234e4534eb53ae9894
Humza <humza1711@gmail.com>
2025-10-25 15:54:55 +0100

    feat(ui): show lucide icons in sidebar (Pages Router) and keep TS variant for App Router

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 // pages/_app.js
 import "@/styles/globals.css";
-import Sidebar from "@/components/Sidebar";
+import Sidebar from "../components/ProfileAISidebar"; // ⬅ use this exact file
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  // (Optional) prevent FOUC when Tailwind dark mode is used, safe to keep
+  useEffect(() => {}, []);
   return (
     <div className="min-h-screen flex">
       <Sidebar />
-      <main className="flex-1 p-6">
+      <main className="flex-1 p-6 lg:p-8">
         <Component {...pageProps} />
       </main>
     </div>
