088219d80bbed3fa4f146b965c2ae7e34251f498
Humza <humza1711@gmail.com>
2025-10-26 17:21:23 +0000

    fix(ui): light theme + correct /my-answers route; add sidebar icons; tailwind scan + tsconfig

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,14 @@
 // pages/_app.js
-import "../styles/globals.css";
+import "../styles/globals.css"; // use the /styles file, not alias
 import Sidebar from "@/components/Sidebar";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="min-h-screen bg-gray-50 text-gray-900">
-      <div className="mx-auto max-w-7xl flex">
-        <Sidebar />
-        <main className="flex-1 p-6">
-          <Component {...pageProps} />
-        </main>
-      </div>
+    <div className="min-h-screen flex bg-white text-gray-900">
+      <Sidebar />
+      <main className="flex-1 min-w-0">
+        <Component {...pageProps} />
+      </main>
     </div>
   );
 }
